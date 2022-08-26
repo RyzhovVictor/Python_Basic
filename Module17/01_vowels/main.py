b77964b10b97830fe0aaab8be5762c507a581fe4
RyzhovVictor <ryzhov.dur@gmail.com>
2022-08-26 12:37:50 +0300

    Module17| Task01| Finish!

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+user_text = list(input('Введите текст: '))
+count = 0
+vowels = ['а', 'о', 'у', 'ы', 'э', 'е', 'ё', 'и', 'ю', 'я']
+user_vowels = []
+
+for letter in user_text:
+    for find_letter in vowels:
+        if letter == find_letter:
+            count += 1
+            user_vowels.append(find_letter)
+
+print('\nСписок гласных букв: ', user_vowels)
+print('Длина списка: ', count)

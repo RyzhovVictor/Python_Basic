cc36e16c7dcd133af209efdc0d9c2393f33d08ad
RyzhovVictor <ryzhov.dur@gmail.com>
2022-08-29 18:30:47 +0300

    Module17| Task06| Finish!

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+user_input = int(input('Кол-во чисел в списке: '))
+
+create_list = [int(random.randint(0, 2)) for i in range(user_input)]
+print('Список до сжатия:', create_list)
+
+create_new_list = [us_list for us_list in create_list if us_list > 0]
+print('Список после сжатия:', create_new_list)

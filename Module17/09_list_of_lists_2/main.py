44a4ca6e4017b954b47df00db46ea19ee7c656a2
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-08-30 21:48:37 +0300

    Module17| Task09| Finish!

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,7 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+disclose = [finish_value for next_list in nice_list
+            for next_value in next_list for finish_value in next_value]
+
+print(disclose)

4ea2ab62164608bf749015dcc0d32b7291178128
RyzhovVictor <ryzhov.dur@gmail.com>
2022-08-30 22:30:32 +0300

    Module17| Task10| Finish!

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,3 +1,24 @@
-alphabet = list(''.join([chr(i) for i in range(ord('а'), ord('а') + 32)]))
+alphabet_ru = ''.join([chr(i) for i in range(ord('а'), ord('я') + 1)])
+string = ''
 
-print(alphabet)
+
+def encryption(symbl, offset, list_alphabet):
+    index = list_alphabet.index(symbl)
+    print(index)
+    if index + offset > len(list_alphabet) - 1:
+        index = index + offset - len(list_alphabet)
+    else:
+        index += offset
+    return index
+
+
+text = input('Введите сообщение: ').lower()
+shift = int(input('Введите сдвиг: '))
+
+for symbol in text:
+    if symbol in alphabet_ru:
+        string += alphabet_ru[encryption(symbol, shift, alphabet_ru)]
+    else:
+        string += symbol
+
+print(string)

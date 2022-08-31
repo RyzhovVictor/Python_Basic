d979d718d3948d1a689c2ccc883c16025de7037d
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-01 00:00:44 +0300

    Module18| Task05| Finish!

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+
+while True:
+    user_password = input('Придумайте пароль: ')
+    num = [int(i) for i in user_password if i.isdigit()]
+    if len(user_password) < 8:
+        print('Пароль ненадежный. Попробуйте еще раз.')
+    elif user_password.islower():
+        print('Пароль ненадежный. Попробуйте еще раз.')
+    elif len(num) < 3:
+        print('Пароль ненадежный. Попробуйте еще раз.')
+    else:
+        print('Это надежный пароль!')
+        break

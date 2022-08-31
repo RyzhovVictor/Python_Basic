aecc461fc3ed3335baf13d408cecf8b2eea43848
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-08-31 22:39:34 +0300

    Module18| Task03| Finish

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+file_name = input('Название файла: ')
+
+spec_symbol = '@№$%^&\(*)'
+
+if not file_name.endswith('.txt') and not file_name.endswith('.docx'):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+elif file_name[0] in spec_symbol:
+    print('Ошибка: название начинается на один из спецаильных символов.')
+else:
+    print('Файл назван верно.')

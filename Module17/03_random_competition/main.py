b74a93d46f7397f64158227f7813cc07b014ce7c
RyzhovVictor <ryzhov.dur@gmail.com>
2022-08-31 13:55:08 +0300

    Module17| Task03| Re-finish!

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,22 +1,12 @@
 import random
 
-second_competitors = []
-first_competitors = []
-winners = []
-
-for competitors in range(1, 21):
-    firsts = round(random.uniform(1, 20), 2)
-    seconds = round(random.uniform(1, 20), 2)
-    first_competitors.append(firsts)
-    second_competitors.append(seconds)
-    if firsts > seconds:
-        winners.append(firsts)
-    else:
-        winners.append(seconds)
-
+first_competitors = [round(random.uniform(1, 20), 2) for first_team in range(1, 21)]
+second_competitors = [round(random.uniform(1, 20), 2) for second_team in range(1, 21)]
+# Ниже - вариант по короче
+# winners = [max(i) for i in zip(first_competitors, second_competitors)]
+winners = [first_competitors[i] if first_competitors[i] > second_competitors[i] else second_competitors[i] for i in
+           range(len(first_competitors))]
 
 print('Первая команда: ', first_competitors)
-print('Первая команда: ', second_competitors)
+print('Вторая команда: ', second_competitors)
 print('Победители тура: ', winners)
-
-

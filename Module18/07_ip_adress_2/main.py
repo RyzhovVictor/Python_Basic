e00c03af2ba335a4fc1dc98e01d898014759b7e8
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-01 17:47:17 +0300

    Module18| Task07| Finish!

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,9 +1,29 @@
 user_input = input('Введите IP: ').split('.')
-print(user_input)
 
-for i in user_input:
-    print(i)
-    # if not user_input.isdigit():
-    #     print('- Это не целое число.')
-    # else:
-    #     print('Все гуд')
+# for i in user_input:
+#     if len(user_input) < 4:
+#         print('Адрес — это четыре числа, разделённые точками.')
+#     elif not i.isdigit():
+#         print(f'{i} - Это не целое число.')
+#     elif int(i) > 255:
+#         print(f'{i} превышает 255.')
+#     else:
+#         print('IP-адрес корректен.')
+
+if len(user_input) < 4:
+    print('Адрес — это четыре числа, разделённые точками.')
+else:
+    num = 0
+    out = 0
+    for i in user_input:
+        if i.isdigit():
+            num += 1
+            if int(i) > 255:
+                out += 1
+                print(f'{i} превышает 255.')
+        else:
+            print(f'{i} - Это не целое число.')
+    if out == 0 and num == 4:
+        print('IP-адрес корректен.')
+
+

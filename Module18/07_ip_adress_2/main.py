b8e1e5c95609c6feb706ddef15e03b5e4a3fcab8
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-01 16:30:14 +0300

    Module18| Task07| Save work

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+user_input = input('Введите IP: ').split('.')
+print(user_input)
+
+for i in user_input:
+    print(i)
+    # if not user_input.isdigit():
+    #     print('- Это не целое число.')
+    # else:
+    #     print('Все гуд')

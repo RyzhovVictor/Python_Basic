0d74087cf3fa77ae6f0b1ad2329884c279287306
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-03 17:36:54 +0300

    Module19| Task02| Save work

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+user_count = int(input('Количество стран: '))
+user_country_dict = dict()
+
+for i in range(user_count):
+    user_country = input(f'{i + 1} страна: ').split()
+    user_country_dict = {user_country[0]: user_country[1:]}
+    # print(user_country_dict)
+
+print(user_country_dict)
+

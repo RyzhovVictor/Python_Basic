cc5b21c0412f446331816946997cd2ef9fd4adb3
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-03 21:09:39 +0300

    Module19| Task02| Finish!

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,10 +1,15 @@
 user_count = int(input('Количество стран: '))
-user_country_dict = dict()
+user_country_dict = {}
 
 for i in range(user_count):
     user_country = input(f'{i + 1} страна: ').split()
-    user_country_dict = {user_country[0]: user_country[1:]}
-    # print(user_country_dict)
+    for city in user_country[1:]:
+        user_country_dict[city] = user_country[0]
 
-print(user_country_dict)
-
+for i in range(3):
+    city = input(f'{i + 1} город: ')
+    country = user_country_dict.get(city)
+    if country:
+        print(f'Город {city} расположен в стране {country}.')
+    else:
+        print(f'По городу {city} данных нет.')

6894a7750a10b5d6bceb41fa4b25af52fb0ee8a8
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-04 22:43:26 +0300

    Module19| Task06| Finish!

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+count_user = int(input('Введите колличество пар слов: '))
+syn_dict = dict()
+
+for i in range(count_user):
+    letter_pair = input(f'{i + 1} пара: ').lower().split()
+    syn_dict[letter_pair[0]] = letter_pair[2]
+    syn_dict[letter_pair[2]] = letter_pair[0]
+
+while True:
+    user_input_letter = input('Введите слово: ').lower()
+    if user_input_letter not in syn_dict:
+        print('Такого слова в словаре нет.')
+    else:
+        print(f'Синоним: {syn_dict[user_input_letter].capitalize()}')
+        break

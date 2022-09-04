867deba1cae2be37b4bdfeab8ad3403465f7dadb
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-04 14:59:55 +0300

    Module19| Task05| Finish

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def histogram(string):
+    sym_dict = dict()
+    for sym in string:
+        if sym in sym_dict:
+            sym_dict[sym] += 1
+        else:
+            sym_dict[sym] = 1
+    return sym_dict
+
+
+user_input = input('Введите текст: ')
+hist = histogram(user_input)
+
+
+def swap_dict(d):
+    new_hist = {}
+    for key, values in d.items():
+        new_hist[values] = new_hist.get(values, []) + [key]
+    return new_hist
+
+
+for i_sym in sorted(swap_dict(hist).keys()):
+    print(f'{i_sym} : {swap_dict(hist)[i_sym]}')

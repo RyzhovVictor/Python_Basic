0f1f8d35bcb12c4b7c50e368835bd7b11e723a4e
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-05 20:22:24 +0300

    Module19| Task07| Finish!

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+user_input = int(input('Введите количество заказов: '))
+orders_dict = dict()
+
+for i_order in range(user_input):
+    orders = input(f'{i_order + 1} заказ: ').split()
+    if orders[0] not in orders_dict:
+        orders_dict[orders[0]] = {orders[1]: orders[2]}
+    else:
+        orders_dict[orders[0]].update({orders[1]: orders[2]})
+
+for name, order in sorted(orders_dict.items()):
+    print(f'{name}: ')
+    for pizza, count in sorted(order.items()):
+        print('\t' f'{pizza}: {count}')
+
+

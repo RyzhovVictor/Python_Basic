835c9101ca9e75487466d819ee5e987a0d6f3532
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-06 12:32:31 +0300

    Module19| Task10| Finish!

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+user_input = input('Введите строку: ')
+
+
+def is_palindrome(text):
+    char_dict = dict()
+    for i_sym in text:
+        char_dict[i_sym] = char_dict.get(i_sym, 0) + 1
+    count = 0
+    for i_value in char_dict.values():
+        if i_value % 2 != 0:
+            count += 1
+    return count <= 1
+
+
+if is_palindrome(user_input):
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')

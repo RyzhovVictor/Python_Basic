7f000edc97b2051526f6dfef107162129ad4639a
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-08 10:07:38 +0300

    Module20| Task02| Finish

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def return_elem(obj_iter):
+    return [i_iter for i_iter, value in enumerate(obj_iter) if is_prime(i_iter)]
+
+
+def is_prime(num):
+    if num >= 2:
+        result = True
+        for i in range(2, num):
+            if num % i == 0:
+                result = False
+                break
+    else:
+        result = False
+    return result
+
+
+def crypto(text):
+    i_list = return_elem(text)
+    elem = []
+    for i_index in i_list:
+        elem.append(text[i_index])
+    return elem
+
+
+print(crypto([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto('О Дивный Новый мир!'))
+

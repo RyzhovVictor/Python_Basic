0ca77a24768431018349bd57d0d55bf62c81fdbb
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-08 15:50:34 +0300

    Module20| Task08| Save work

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+phonebook = {}
+
+
+
+def add_contact(phone_dict):
+    name = input('Введите имя и фамилию нового контакта (через пробел): ').split()
+    phonenum = input('Введите номер телефона: ')
+    phone_dict[name]: phonenum
+    phonebook['name']: 1
+    print(phone_dict)
+
+
+add_contact(phonebook)
+
+
+def find_contact(surname):
+    pass

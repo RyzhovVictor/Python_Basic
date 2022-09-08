70f0fab578d15ecc7be2554559f4482f05cc6b22
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-08 11:40:41 +0300

    Module20| Task03| Finish

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+any_elem = 2
+slicer = (1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10)
+
+
+def add_tuple(any_tuple, elem):
+    if elem in any_tuple:
+        if any_tuple.count(elem) > 1:
+            first_index = any_tuple.index(elem)
+            second_index = any_tuple.index(elem, first_index + 1) + 1
+            return any_tuple[first_index:second_index]
+        else:
+            return any_tuple[any_tuple.index(elem):]
+    else:
+        return ()
+
+
+print(add_tuple(slicer, any_elem))

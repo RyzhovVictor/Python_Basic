88624d8a44329f99fb4d9b198e9ed5f1cf5c8f5e
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-08 09:31:40 +0300

    Module20| Task01| Finish

--- Module20/01_code_review/main.py
+++ Module20/01_code_review/main.py
@@ -21,25 +21,40 @@ students = {
 
 
 def accept_arg(dict_students):
-    lst = []
-    string = ''
-    for i in dict_students:
-        lst += (dict_students[i]['interests'])
-        string += dict_students[i]['surname']
-    cnt = 0
-    for s in string:
-        cnt += 1
-    return lst, cnt
+    id_and_age = [(id_student, age_student['age'])
+                  for id_student, age_student in dict_students.items()]
+
+    interest_student = {interests
+                        for in_dict in dict_students.values()
+                        for interests in in_dict['interests']}
+
+    all_length = [values
+                  for in_dict in dict_students.values()
+                  for length, values in enumerate(in_dict['surname'])]
+
+    print(f'Список пар "ID студента - возраст": {id_and_age}')
+    print(f'Полный список интересов всех студентов: {interest_student}')
+    print(f'Общая длина всех фамилий студентов: {len(all_length)}')
 
 
-pairs = []
-for i in students:
-    pairs += (i, students[i]['age'])
+accept_arg(students)
 
-my_lst = accept_arg(students)[0]
-l = accept_arg(students)[1]
 
-print(f'Список пар "ID студента - возраст": {pairs}')
-print(f'Полный список интересов всех студентов: {my_lst}')
-print(f'Общая длина всех фамилий студентов: {l}')
-
+# def accept_arg(dict_students):
+#     lst = []
+#     string = ''
+#     for i in dict_students:
+#         lst += (dict_students[i]['interests'])
+#         string += dict_students[i]['surname']
+#     cnt = 0
+#     for s in string:
+#         cnt += 1
+#     return lst, cnt
+#
+#
+# pairs = []
+# for i in students:
+#     pairs += (i, students[i]['age'])
+#
+# my_lst = accept_arg(students)[0]
+# l = accept_arg(students)[1]

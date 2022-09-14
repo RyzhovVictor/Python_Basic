a27527359fb86c441437627081406a7ab35f17d8
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-09-14 08:41:30 +0300

    Module20| Task09| Finish

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -2,24 +2,30 @@ total_quan_str_protocols = int(input('Сколько записей вносит
 protocols = {}
 
 
+def score_key(a):
+    return a[1][0] * 100000000 - a[1][1]
+
+
 def protocol_input_data():
     print('Записи (результат и имя):')
     for i_rec in range(1, total_quan_str_protocols + 1):
-        records = input(f'{i_rec}-я запись: ').split()
-        if int(records[0]) / float(records[0]) == 1.0 and int(records[0]) > 0:
-            protocols.setdefault(records[0], []).append(records[1])
+        point, name = input(f'{i_rec}-я запись: ').split()
+        point = int(point)
+        if name in protocols:
+            if point > protocols[name][0]:
+                protocols[name][0] = point
+                protocols[name][1] = i_rec
         else:
-            print('Результат участника должен быть целым и неотрициательным числом')
-            break
+            protocols[name] = [point, i_rec]
 
 
 def competitions_result():
-    for i_point, i_name in sorted(protocols.items()):
-        for i_sep in i_name:
-            print(i_sep, i_point)
-# TODO функция, которая выше.
-# Не могу дальше разобраться, как правильно отсортировать и правильно вывести.
-# Может я не так начал все делать.
+    protocol = list(protocols.items())
+    protocol.sort(key=score_key, reverse=True)
+    print('\n', 'Итоги соревнований: ')
+    for winner_index in range(3):
+        print(f'{winner_index + 1}-е место {protocol[winner_index][0]}', end=' ')
+        print(f'({protocol[winner_index][1][0]})', sep='')
 
 
 protocol_input_data()

53dd18770208871212f7149ae4c086d2aa54acf9
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-18 01:07:41 +0300

    Module21| Task02| Finish!

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,4 +1,4 @@
-# a = [1, 2, 3]
+# a = [1, 2, 3, 5]
 # b = '456'
 # c = (7, 8, 9)
 
@@ -6,19 +6,17 @@
 # b = {1: "s", 2: "q", 3: 4}
 # c = (1, 2, 3, 4, 5)
 
-a = [{"x": 4}, "b", "z", "d"]
-b = (10, {20, }, [30], "z")
+# a = [{"x": 4}, "b", "z", "d"]
+# b = (10, {20, }, [30], "z")
 
 
 def my_zip(*args):
-    # args = list(map(list, args))
-    return ((i_elem[j_elem] for i_elem in args) for j_elem in range(len(args)))
+    min_args = min(map(len, args))
+    args = list(map(list, args))
+    return [tuple(i_elem[k_elem] for i_elem in args) for k_elem in range(min_args)]
 
 
-unzip = my_zip(a, b)
+unzip = my_zip(a, b, c)
 
 print('Результат:')
-
-for i in unzip:
-    print(*i)
-
+print(*unzip)

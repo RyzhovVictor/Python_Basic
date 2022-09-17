534449794d16889f48fc14a81abcecd1c3eebd80
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-17 18:47:09 +0300

    Module21| Task02| Need help!

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,11 +1,24 @@
-a = [{"x": 4}, "b", "z", "d"]
+# a = [1, 2, 3]
+# b = '456'
+# c = (7, 8, 9)
 
+# a = [1, 2, 3, 4, 5]
+# b = {1: "s", 2: "q", 3: 4}
+# c = (1, 2, 3, 4, 5)
+
+a = [{"x": 4}, "b", "z", "d"]
 b = (10, {20, }, [30], "z")
 
 
 def my_zip(*args):
-    zip_on = [i_elem[0] for i_elem in args]
-    print(zip_on)
+    # args = list(map(list, args))
+    return ((i_elem[j_elem] for i_elem in args) for j_elem in range(len(args)))
 
 
-my_zip(a, b)
+unzip = my_zip(a, b)
+
+print('Результат:')
+
+for i in unzip:
+    print(*i)
+

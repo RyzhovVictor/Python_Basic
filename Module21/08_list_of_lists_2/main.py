14128bc326e69feab78ea7f0dffbe30f7819b465
RyzhovVictor <ryzhov.dur@gmail.com>
2022-09-19 17:20:32 +0300

    Module21| Task08| Finish

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def slicing(args):
+    if not args:
+        return args
+    if isinstance(args[0], list):
+        return slicing(args[0]) + slicing(args[1:])
+    return args[:1] + slicing(args[1:])
+
+
+print(slicing(nice_list))

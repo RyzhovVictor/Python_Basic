bef600816b37582e3ec6d4f3536b093cc7cc3740
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-10-10 16:22:41 +0300

    Module22| Task02| Finish

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+reverse_list = []
+
+with open('zen.txt', 'r') as file:
+    for line in file:
+        reverse_list.append(line.split('\n'))
+
+
+for i_str in reverse_list[::-1]:
+    print(' '.join(map(str, i_str)))
+

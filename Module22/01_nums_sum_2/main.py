5eaa812ace943104780842bfd3d13bfe6f7f88cd
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-10-10 15:36:01 +0300

    Module22| Task01| Finish

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+summ = 0
+
+with open('numbers.txt', 'r') as file:
+    for line in file:
+        for i_digit in line.split():
+            summ += int(i_digit)
+
+
+with open('answer.txt', 'w') as file:
+    file.write(str(summ))

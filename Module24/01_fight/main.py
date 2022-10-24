6763496a831ae883062a3d29f2b495405369fad1
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-10-24 13:32:50 +0300

    Module24| Task01| Finish

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,30 @@
+import random
 
+
+class Warrior:
+    def __init__(self, name, health=100):
+        self.name = name
+        self.health = health
+
+    def fight(self, unit):
+        unit.health -= 20
+        if unit.health > 0:
+            print(f'{self.name} атаковал {unit.name}. '
+                  f'У {unit.name} осталось {unit.health} здоровья')
+        else:
+            print(f'{self.name} атаковал {unit.name}. {unit.name} убит')
+            unit.health = 0
+        return unit.health
+
+
+warrior_1 = Warrior('Петя')
+warrior_2 = Warrior('Вася')
+warriors = [warrior_1, warrior_2]
+
+while True:
+    attack_index = random.randint(0, 1)
+    target_index = (attack_index + 1) % 2
+    target_health = warriors[attack_index].fight(warriors[target_index])
+    if target_health == 0:
+        print(f'{warriors[attack_index].name} Победил!')
+        break

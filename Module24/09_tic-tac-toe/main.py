55d6ae5a024c62331e2c769101cc93521f1a111a
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-11-25 12:23:48 +0300

    Module24| Task09| Finish

--- Module24/09_tic-tac-toe/main.py
+++ Module24/09_tic-tac-toe/main.py
@@ -1,54 +1,55 @@
-board = list(range(1, 10))
-wins_coords = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+class TicTacToe:
+    board = list(range(1, 10))
+    wins_coords = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
 
+    def draw_board(self):
+        print('-------------')
+        for i in range(3):
+            print('|', self.board[0 + i * 3], '|', self.board[1 + i * 3], '|', self.board[2 + i * 3], '|')
+        print('-------------')
 
-def draw_board():
-    print('-------------')
-    for i in range(3):
-        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
-    print('-------------')
+    def take_input(self, player_token):
+        while True:
+            value = input(f'Куда поставить: {player_token} ? ')
+            if not (value in '123456789'):
+                print('Ошибочный ввод. Повторите.')
+                continue
+            value = int(value)
+            if str(self.board[value - 1]) in 'XO':
+                print('Эта клетка уже занята')
+                continue
+            self.board[value - 1] = player_token
+            break
 
-
-def take_input(player_token):
-    while True:
-        value = input(f'Куда поставить: {player_token} ? ')
-        if not (value in '123456789'):
-            print('Ошибочный ввод. Повторите.')
-            continue
-        value = int(value)
-        if str(board[value - 1]) in 'XO':
-            print('Эта клетка уже занята')
-            continue
-        board[value - 1] = player_token
-        break
-
-
-def check_win():
-    for each in wins_coords:
-        if (board[each[0] - 1]) == (board[each[1] - 1]) == (board[each[2] - 1]):
-            return board[each[1] - 1]
-    else:
-        return False
+    def check_win(self):
+        for each in self.wins_coords:
+            if (self.board[each[0] - 1]) == (self.board[each[1] - 1]) == (self.board[each[2] - 1]):
+                return self.board[each[1] - 1]
+        else:
+            return False
 
 
 def main():
+    game = TicTacToe()
     counter = 0
     while True:
-        draw_board()
+        game.draw_board()
         if counter % 2 == 0:
-            take_input('X')
+            game.take_input('X')
         else:
-            take_input('O')
+            game.take_input('O')
         if counter > 3:
-            winner = check_win()
+            winner = game.check_win()
             if winner:
-                draw_board()
-                print(winner,  'выиграл!')
+                game.draw_board()
+                print(winner, 'Выиграл!')
                 break
         counter += 1
         if counter > 8:
-            draw_board()
+            game.draw_board()
             print('Ничья!')
             break
 
-main()
+
+if __name__ == '__main__':
+    main()

d3e4415270631a155e95135f969c6c8857e6386d
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-11-25 14:46:13 +0300

    Module24| Task02| Corrected

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -4,6 +4,12 @@ class Students:
         self.num_group = num_group
         self.progress = progress
 
+    def give_average(self) -> float:
+        return round(sum(self.progress) / len(self.progress), 2)
+
+    def __str__(self):
+        return f'ФИ: {self.name} - № группы: {self.num_group} - Средний балл: {self.give_average()}'
+
 
 student_1 = Students('Вася Пупкин', 1, [2, 3, 4, 2, 5])
 student_2 = Students('Алеся Петрова', 2, [3, 4, 5, 5, 2])
@@ -19,12 +25,8 @@ student_10 = Students('Таганрок Прибалтийский', 10, [5, 4, 
 all_students = [student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9,
                 student_10]
 
-avg_list = []
-
+all_students.sort(key=lambda x: x.give_average())
 for student in all_students:
-    sum_lst = sum(student.progress)
-    str_avg = sum_lst / len(student.progress)
-    avg_list.append(str_avg)
+    print(student)
 
-print(sorted(avg_list))
 

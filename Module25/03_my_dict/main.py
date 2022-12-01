8dc5f62638da8b9d7d5e055be11454249f06edfe
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-12-01 20:36:56 +0300

    Module25| Task03| Finish

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+class NewDict(dict):
+    def get(self, key, default_value=0):
+        if key in self.keys():
+            return self[key]
+        else:
+            return default_value
+
+
+new_dict = NewDict({'Васька': 35, 'Женька': 45, 'Вовка': 60})
+print(f"Васька: {new_dict.get('Васька')}")
+print(f"Ванька : {new_dict.get('Ванька')}")
+

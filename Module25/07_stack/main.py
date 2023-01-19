2ac1ba08e2361bf1319a063837873821e4d5b9ab
RyzhovVictor <ryzhov.dur@gmail.com>
2023-01-19 15:03:09 +0300

    Module25| Task07| Finish

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -1,66 +1,52 @@
-from dataclasses import dataclass, field
-
-
-@dataclass(order=True)
-class TaskData:
-    sort_index: int = field(init=False, repr=False)
-
-    name: str
-    index: int
-
-    def __post_init__(self):
-        self.sort_index = self.index
-
-
 class Stack:
     def __init__(self):
         self.stack = []
 
-    def pop(self, index: int = -1):
+    def __str__(self):
+        return '; '.join(self.stack)
+
+    def pop(self):
         if len(self.stack) == 0:
             return None
-        return self.stack.pop(index)
+        return self.stack.pop()
 
-    def push(self, item):
-        self.stack.append(item)
+    def push(self, elem):
+        self.stack.append(elem)
 
 
 class TaskManager:
-    stack = Stack()
+    def __init__(self):
+        self.task = dict()
 
     def __str__(self):
-        result = ''
-        old_index: int = 0
-        self.stack.stack.sort()
-        result += 'Результат:'
-        for task in self.stack.stack:
-            if old_index == task.index:
-                result += f'; {task.name}'
-            else:
-                result += f'\n{task.index} {task.name}'
-                old_index = task.index
-        return result
+        display = []
+        if self.task:
+            for i_priority in sorted(self.task.keys()):
+                display.append(f'{str(i_priority)}: {self.task[i_priority]}.\n')
+        return ''.join(display)
 
-    def new_task(self, name: str, index: int):
-        self.stack.push(TaskData(name, index))
+    def new_task(self, task, priority):
+        if priority not in self.task:
+            self.task[priority] = Stack()
+        self.task[priority].push(task)
 
-    def delete_task(self, name: str, index: int):
-        index_element = 0
-        for item in self.stack.stack:
-            if item.name == name and item.index == index:
-                self.stack.pop(index_element)
-                break
-            index_element += 1
+    def delete_task(self, task, priority):
+        if priority not in self.task.keys() and task not in self.task.keys():
+            print("Задача с таким приоритетом нет!")
+        else:
+            self.task[priority].pop()
 
 
 manager = TaskManager()
+manager.new_task('Украсить елку', 4)
 manager.new_task('Сделать уборку', 4)
 manager.new_task('Помыть посуду', 4)
 manager.new_task('Отдохнуть', 1)
 manager.new_task("поесть", 2)
 manager.new_task("сдать дз", 2)
+manager.new_task("Лечь", 3)
 print(manager)
 manager.delete_task("поесть", 2)
 manager.delete_task('Сделать уборку', 4)
+manager.delete_task('Украсить елку', 4)
 print(manager)
-
